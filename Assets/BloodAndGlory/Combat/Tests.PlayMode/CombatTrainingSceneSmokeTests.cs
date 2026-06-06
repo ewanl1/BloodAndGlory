8fbb585c7d9db345a0014399b0996a8525cbcda3
ewanl1 <ewanlynch1@outlook.com>
2026-06-06 16:07:00 +0100

    test: add combat training scene smoke test

---
+++ Assets/BloodAndGlory/Combat/Tests.PlayMode/CombatTrainingSceneSmokeTests.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.TestTools;
+
+namespace BloodAndGlory.Combat.Tests.PlayMode
+{
+    public sealed class CombatTrainingSceneSmokeTests
+    {
+        [UnityTest]
+        public IEnumerator CombatTrainingScene_Loads()
+        {
+            yield return SceneManager.LoadSceneAsync("CombatTrainingScene", LoadSceneMode.Single);
+            var scene = SceneManager.GetActiveScene();
+
+            Assert.AreEqual("CombatTrainingScene", scene.name);
+            Assert.IsNotNull(GameObject.Find("Training Floor"));
+            Assert.IsNotNull(GameObject.Find("Combat Debug Overlay"));
+        }
+    }
+}
